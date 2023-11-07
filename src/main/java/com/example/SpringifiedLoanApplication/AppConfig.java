5a7e36217f4fa5ecd66171a69cbe5731032ba097
Sarah-Irvine <123417453+Sarah-Irvine@users.noreply.github.com>
2023-11-07 11:10:19 +0000

    Added AppConfig class

---
+++ src/main/java/com/example/SpringifiedLoanApplication/AppConfig.java
@@ -0,0 +1,8 @@
+package com.example.SpringifiedLoanApplication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
--- src/main/java/com/example/SpringifiedLoanApplication/SpringifiedLoanApplication.java
+++ src/main/java/com/example/SpringifiedLoanApplication/SpringifiedLoanApplication.java
@@ -2,12 +2,15 @@ package com.example.SpringifiedLoanApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class SpringifiedLoanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringifiedLoanApplication.class, args);
+		//SpringApplication.run(SpringifiedLoanApplication.class, args);
+		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
 	}
 
 }
